fa886fc92618741cc93365d05aa1871186a0e9b3
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-07-03 20:26:13 +0900

    Revert "validate-seven-edit"
    This reverts commit 9bc4565fae365fa23ffb49820f4608633c143af6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 7..128
-        #  password_length: 7..128は登録ログイン時にパスワード７文字以上の表示を設定
+         :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true, uniqueness: true, length: { minimum: 7}
-  # length: { minimum: 7}は最低７文字必須のバリデーションを設定（大森，阿部）
 end
