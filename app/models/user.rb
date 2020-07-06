b75e159b6915bed48da8351c685a1a1ecb1585f7
habe11 <aced119@gmail.com>
2020-07-06 20:08:52 +0900

    validation-edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :encrypted_password, presence: true, length: { minimum: 7}
   # length: { minimum: 7}は最低７文字必須のバリデーションを設定（大森，阿部）
+  validates :first_name, :family_name, :first_kana, :family_kana, presence: true,
+              format: {
+                with: /\A[ぁ-んァ-ン一-龥]/,
+                # 全角(平仮名、カタカナ、漢字)のみ入力（大森，阿部）
+                message: "全角で入力して下さい"
+              }
+            
+
 end
