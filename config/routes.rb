8ba32551eed2a868fb2678661f0aed7d55b88f8f
kotayamazaki <kota80gh11@icloud.com>
2020-07-07 17:38:26 +0900

    products_controller_buy_haml_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :edit, :update]
-  root 'items#index'
+  resources :products, only: [:index]
+  root 'products#index'
 end
