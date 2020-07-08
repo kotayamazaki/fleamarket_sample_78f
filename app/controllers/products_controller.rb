fb904e8128a2da2440fc7cfa79caedaf532a6c75
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-07-08 19:10:02 +0900

    product-exhibition-edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,23 +2,33 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    # @product.users << current_user
+    # @product.seller << current_user
+    # @image = Image.new
   end
 
   def create
     @product = Product.new(product_params)
     # binding.pry
-    @product.save
-    # @product.errors.messages
-    # if @group.save
-    #   redirect_to root_path, notice: 'グループを作成しました'
-    # else
-    #   render :new
-    # end
+    if @product.save 
+      # @image.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def product_params
-    params.require(:product).permit(:name, :name, :text, :category, :condition, :postage, :days, :area, :price)
+    params.require(:product).permit(:name, 
+                                    # :name, 
+                                    :text, 
+                                    :category_id, 
+                                    :condition, 
+                                    :postage, 
+                                    :days, 
+                                    :area, 
+                                    :price,
+                                    image_attributes: [:name, :product_id]
+                                    ).merge(seller_id: current_user.id)
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   # belongs_to :user
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: :true
   #一つのモデルにアソシエーションを複数付けたい時は， belongs_to :カラム名, class_name: "モデル名"　と記述
-  has_many :images
+  has_many :images, dependent: :destroy
   belongs_to :category
+  accepts_nested_attributes_for :image
 end
