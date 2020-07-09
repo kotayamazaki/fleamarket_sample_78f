83f0a6d6cffa24425e0d45d148f9eb9006f2b9ee
YosukeTakahashi <y.takahashi0813@gmail.com>
2020-07-09 18:53:39 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'card/new'
-  get 'card/show'
   resources :users, only: [:index, :edit, :update, :show, :destroy]
   resources :products, only: [:index]
   resources :card, only: [:new, :show] 
