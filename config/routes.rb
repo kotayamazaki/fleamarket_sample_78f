4b99fa4ffcac4b3aaed1055e7e22bc3125530de1
Tamadu <66250516+Tamadu@users.noreply.github.com>
2020-07-09 17:16:17 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'card/new'
+  get 'card/show'
   resources :users, only: [:index, :edit, :update, :show, :destroy]
   resources :products, only: [:index]
-  root 'devise/sessions#destroy'
+  resources :card, only: [:new, :show] 
+  root 'products#index'
 end
