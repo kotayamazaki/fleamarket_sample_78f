06ceb88f0f2ce23910da0dd48530699a57ad4b61
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-07-10 10:48:58 +0900

    form-View-edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :encrypted_password, presence: true, length: { minimum: 7}
   # length: { minimum: 7}は最低７文字必須のバリデーションを設定（大森，阿部）
+  # presence: true　は空だと保存できない
   validates :first_name, :family_name, :first_kana, :family_kana, presence: true,
               format: {
                 with: /\A[ぁ-んァ-ン一-龥]/,
@@ -18,4 +19,7 @@ class User < ApplicationRecord
   has_one :address
   has_many :products
   # belongs_to :cards
+  # has_many :buyed_products, foreign_key: "buyer_id", class_name: "Item"
+  # has_many :saling_products, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Product"
+  # has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
 end
