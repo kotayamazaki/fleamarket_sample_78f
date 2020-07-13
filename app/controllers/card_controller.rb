3a45e056790a38127d6903dacf0ba9d12621212f
Tamadu <66250516+Tamadu@users.noreply.github.com>
2020-07-13 15:49:31 +0900

    edit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -15,7 +15,7 @@ class CardController < ApplicationController
         # email: current_user.email, #なくてもOK
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+        ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
@@ -25,10 +25,10 @@ class CardController < ApplicationController
     end
   end
 
-  before_action :set_card, only: [:delete, :show]
+  # before_action :set_card, only: [:delete, :show]
 
   def delete #PayjpとCardデータベースを削除します
-    # card = Card.find_by(user_id: current_user.id)
+    card = Card.find_by(user_id: current_user.id)
     if card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
@@ -39,9 +39,9 @@ class CardController < ApplicationController
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    # card = Card.find_by(user_id: current_user.id)
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
-      redirect_to action: "new" 
+    redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
@@ -49,9 +49,9 @@ class CardController < ApplicationController
     end
   end
 
-  private
-  def set_card 
-    card = Card.find_by(user_id: current_user.id)
-  end
+  # private
+  # def set_card 
+  #   card = Card.find_by(user_id: current_user.id)
+  # end
 
 end
