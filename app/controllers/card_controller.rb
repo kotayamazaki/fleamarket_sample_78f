679c50782bb0bea7d39e3197a2d5fb9bb3c813a7
YosukeTakahashi <y.takahashi0813@gmail.com>
2020-07-13 19:31:55 +0900

    edit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -16,7 +16,7 @@ class CardController < ApplicationController
         # email: current_user.email, #なくてもOK
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
-        ) #念の為metadataにuser_idを入れましたがなくてもOK
+      ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
@@ -28,30 +28,29 @@ class CardController < ApplicationController
 
   def delete #PayjpとCardデータベースを削除します
     # card = Card.find_by(user_id: current_user.id)
-    if card.present?
+    if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
       redirect_to action: "new"
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
     # card = Card.find_by(user_id: current_user.id)
-    if card.blank?
+    if @card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   private
   def set_card 
-    card = Card.find_by(user_id: current_user.id)
-    binding.pry
+    @card = Card.find_by(user_id: current_user.id)
   end
 
 end
