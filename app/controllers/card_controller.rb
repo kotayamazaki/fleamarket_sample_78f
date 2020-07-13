d467c24d4f234443a592a3031b725150864a7285
Tamadu <66250516+Tamadu@users.noreply.github.com>
2020-07-13 12:53:45 +0900

    edit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,7 +2,7 @@ class CardController < ApplicationController
   require "payjp"
 
   def new
-    redirect_to action: "show"if Card.exists?(user_id: current_user.id)
+    redirect_to action: "show" if Card.exists?(user_id: current_user.id)
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
@@ -11,10 +11,10 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      description: '登録テスト', #なくてもOK
-      # email: current_user.email, #なくてもOK
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
+        description: '登録テスト', #なくてもOK
+        # email: current_user.email, #なくてもOK
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
@@ -41,7 +41,7 @@ class CardController < ApplicationController
   def show #Cardのデータpayjpに送り情報を取り出します
     # card = Card.find_by(user_id: current_user.id)
     if card.blank?
-    redirect_to action: "new" 
+      redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update, :show, :destroy]
   resources :products, only: [:index]
   root 'items#index'
-  resources :card, only: [:new, :show] do
+  resources :card, only: [:new, :show, :destroy] do
     collection do
-      post 'show', to: 'card#show'
+      get 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
     end
   end
-  # resources :products, except: :show
-  # resources :card, only: [:new, :show] 
-  # root 'items#index'
 end
