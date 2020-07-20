fed6ad14abaf585637cc65a29ca87af084cbc49b
kotayamazaki <kota80gh11@icloud.com>
2020-07-20 20:34:52 +0900

    routes.rb_edit_carrierwave_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :users, only: [:index, :edit, :update, :show, :destroy]
-  resources :categories, only: [:index, :show]
-  post  'categories', to: 'products#select_category_index'
   resources :products do
     member do
       get 'buy', to: 'products#buy'
-      post 'index', to: 'purchase#index'
-      get 'done', to: 'purchase#done'
+    end
+    resources :purchase do
+      member do
+        get 'index', to: 'purchase#index'
+        get 'done', to: 'purchase#done'
+      end
     end
   end
+  # productsのルーティングを見直して、ネストの構造をさらに追加すると購入画面でエラーが出た。
+  
+  resources :users, only: [:index, :edit, :update, :show, :destroy]
+  resources :categories, only: [:index, :show]
+  post  'categories', to: 'products#select_category_index'
+
   resources :card, only: [:new, :destroy] do
     collection do
       get 'mypage', to: 'card#mypage'
