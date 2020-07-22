80972f48659803ac762d7a9845eed5963d2ea587
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-07-22 12:03:16 +0900

    add:product-show-edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -95,6 +95,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @products = Product.includes(:images).limit(1).order('created_at DESC')
+    @category_id = @product.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
   end
 
   def buy
